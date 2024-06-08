ef27b41dc597619a66ac0824e7cbb74490e123ca
Sai Ram pinjarla <sairampinjarla@Sais-MacBook-Air.local>
2024-06-09 00:16:43 +0530

    Optimised code

---
+++ README.md
@@ -0,0 +1,54 @@
+# Final Spinner
+
+A fun and interactive spinner created using Python's Turtle module. Press the spacebar to flick the spinner and watch it spin!
+
+## Project Description
+
+This project involves creating a spinner using the Turtle graphics library in Python. The spinner responds to user input (spacebar key press) to increase its spinning speed. It gradually slows down due to simulated friction.
+
+## Features
+
+- Interactive spinner that reacts to spacebar presses.
+- Uses the Turtle graphics library to draw and animate the spinner.
+- Adjustable spinner color.
+
+## Getting Started
+
+### Prerequisites
+
+- Python 3.x
+- Turtle module (part of the Python standard library)
+
+### Installation
+
+1. Clone the repository or download the source code.
+    ```sh
+    git clone https://github.com/sairam-penjarla/Fidget-spinner.git
+    ```
+2. Ensure you have the required packages installed:
+
+    ```sh
+    pip install -r requirements.txt
+    ```
+
+### Usage
+
+1. Run the script:
+
+    ```sh
+    python main.py
+    ```
+
+2. Press the spacebar to flick the spinner and make it spin faster.
+
+### Example
+
+The spinner starts in a static position. When you press the spacebar, it will start spinning. Each subsequent spacebar press will increase its speed.
+
+## Contributing
+
+Feel free to fork this repository and contribute by submitting pull requests. Any enhancements, bug fixes, or additional features are welcome.
+
+## License
+
+This project is licensed under the MIT License - see the LICENSE file for details.
--- main.py
+++ main.py
@@ -1,78 +1,66 @@
 import os
 from turtle import *
-class final_spinner:
-  def __init__(self, clr):
-    self.clr = clr
-    state = {'turn': 0}
-    def main_spinner():
-        def spinner():
-            clear()
-            dot(130, 'black')
-            dot(120, self.clr)
-            angle = state['turn'] / 10
-            right(angle)
-            stick_len = 110
-            forward(stick_len)
-            dot(130, 'black')
-            dot(120, self.clr)
-            dot(100, 'gray')
-            dot(95, self.clr)
-            dot(93, 'black')
-            dot(90, 'gray')
-            dot(70, 'black')
-            dot(60, 'white')
-            back(stick_len)
-            right(120)
-            val = 170
-            forward(stick_len)
-            dot(130, 'black')
-            dot(120, clr)
-            dot(100, 'gray')
-            dot(95, clr)
-            dot(93, 'black')
-            dot(90, 'gray')
-            dot(70, 'black')
-            dot(60, 'white')
-            back(stick_len)
-            right(120)
-            val = 30
-            forward(stick_len)
-            dot(130, 'black')
-            dot(120, clr)
-            dot(100, 'gray')
-            dot(95, clr)
-            dot(93, 'black')
-            dot(90, 'gray')
-            dot(70, 'black')
-            dot(60, 'white')
-            back(stick_len)
-            right(120)
-            dot(110, 'black')
-            dot(100, clr)
-            update()
 
-        def animate():
-            if state['turn'] > 0:
-                state['turn'] -= 1
-            spinner()
-            ontimer(animate, 20)
+class FinalSpinner:
+    def __init__(self, clr):
+        self.clr = clr
+        self.state = {'turn': 0}
+        self.setup_turtle()
+        self.main_spinner()
 
-        def flick():
-            state['turn'] += 15
-
+    def setup_turtle(self):
+        """Set up the turtle graphics environment."""
         setup(420, 420, 370, 0)
         color('white')
         hideturtle()
         width(0)
         tracer(False)
-        onkeypress(flick, 'space')
+        onkeypress(self.flick, 'space')
         listen()
-        animate()
+
+    def spinner(self):
+        """Draw the spinner with the current state."""
+        clear()
+        stick_len = 110
+        angle = self.state['turn'] / 10
+        
+        for _ in range(3):
+            right(angle)
+            self.draw_stick(stick_len)
+            right(120)
+            
+        dot(110, 'black')
+        dot(100, self.clr)
+        update()
+
+    def draw_stick(self, length):
+        """Draw one stick of the spinner."""
+        forward(length)
+        dot(130, 'black')
+        dot(120, self.clr)
+        dot(100, 'gray')
+        dot(95, self.clr)
+        dot(93, 'black')
+        dot(90, 'gray')
+        dot(70, 'black')
+        dot(60, 'white')
+        back(length)
+
+    def animate(self):
+        """Animate the spinner by updating its state."""
+        if self.state['turn'] > 0:
+            self.state['turn'] -= 1
+        self.spinner()
+        ontimer(self.animate, 20)
+
+    def flick(self):
+        """Increase the spinner's turn state."""
+        self.state['turn'] += 15
+
+    def main_spinner(self):
+        """Initialize and start the spinner animation."""
+        self.animate()
         done()
-    main_spinner()
-mydir = "/Users/sai/Desktop/everything/Programming/Major projects/Fidget spinner/color"
-for root, dirs, files in os.walk(mydir):
-    for filename in files:
-        clr = (filename)
-# mention the file path in the 73rd line and put clr in place of 'red' . You can simply put 'blue','yellow' etc inplace of 'red in below line
-p1 = final_spinner('red')
+
+# Create a spinner with a specified color
+FinalSpinner('blue') # red, blue, yellow
